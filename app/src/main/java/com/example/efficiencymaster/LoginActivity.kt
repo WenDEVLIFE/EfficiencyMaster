63c5b84a088896f6fd2f0710677e5783dd374043
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-04-13 08:42:57 +0800

    Changing the color of the background 💻

--- app/src/main/java/com/example/efficiencymaster/LoginActivity.kt
+++ app/src/main/java/com/example/efficiencymaster/LoginActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
