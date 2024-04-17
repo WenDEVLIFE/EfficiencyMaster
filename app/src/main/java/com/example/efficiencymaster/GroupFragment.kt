800832f87559c005674183f926e80cf95a29f86f
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-04-17 09:36:41 +0800

    Added minor backends of the GroupFragment 👾

--- app/src/main/java/com/example/efficiencymaster/GroupFragment.kt
+++ app/src/main/java/com/example/efficiencymaster/GroupFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.Locale
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +26,8 @@ class GroupFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +41,39 @@ class GroupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false)
+        val view = inflater.inflate(R.layout.fragment_group, container, false)
+
+        // Find the ImageButton in the fragment_group.xml layout
+        val ImageButton = view.findViewById<ImageButton>(R.id.imageButton)
+        ImageButton.setOnClickListener {
+
+            // Open the drawer when the ImageButton is clicked
+            val activity = activity as MainActivity
+            activity.OpenDrawer()
+
+        }
+
+        // This is for search functions
+        val searchView = view.findViewById<SearchView>(R.id.search_group)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                var search: String = query?.lowercase(Locale.getDefault()) ?: return false
+                // Add your search logic here
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                var search: String = newText?.lowercase(Locale.getDefault()) ?: return false
+                // Add your search logic here
+                return true
+            }
+        })
+
+        // This will get the recycler view from the fragment_group.xml layout
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.setLayoutManager(LinearLayoutManager(context))
+
+        return view
     }
 
     companion object {
