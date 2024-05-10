06eeddcde17c64e4c347bf1e863c96d90d6711be
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-05-10 09:40:12 +0800

    Done coding the task

--- app/src/main/java/classes/Task.kt
+++ app/src/main/java/classes/Task.kt
@@ -1,3 +1,17 @@
 package classes
 
-data class Task()
+data class Task(
+    var _TaskNamme:String,
+    var _TaskDescription:String,
+
+){
+    val taskname:String
+        get() = _TaskNamme
+
+    val taskdescription:String
+        get() = _TaskDescription
+
+    override fun toString(): String {
+        return "Taskname: $taskname, TaskDescription: $taskdescription"
+    }
+}
