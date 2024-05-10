04a3eb1dd66881f1d07915c1d941c18bcc0d3094
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-05-10 13:16:34 +0800

    Done fixing the progressbar animation and display its task percentage

--- app/src/main/java/com/example/efficiencymaster/HomeFragmentation.kt
+++ app/src/main/java/com/example/efficiencymaster/HomeFragmentation.kt
@@ -16,6 +16,7 @@ import com.github.mikephil.charting.data.BarEntry
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
+import java.time.LocalDate
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -31,6 +32,11 @@ class HomeFragmentation : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var percentage : TextView
+    lateinit var TaskDone : TextView
+    lateinit var circularProgressBar : CircularProgressBar
+    lateinit var circularProgressBar2 : CircularProgressBar
+    lateinit var barChart : BarChart
 
     private var username: String? = null
     private var email: String? = null
@@ -54,7 +60,7 @@ class HomeFragmentation : Fragment() {
         // Inflate the layout for this fragment
     val view = inflater.inflate(R.layout.fragment_home_fragmentation, container, false)
 
-       val percentage = view.findViewById<TextView>(R.id.textView5)
+       percentage = view.findViewById<TextView>(R.id.textView5)
 
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
@@ -74,22 +80,12 @@ class HomeFragmentation : Fragment() {
         }
 
         val entries = ArrayList<BarEntry>()
-        entries.add(BarEntry(1f, 2f))
-        entries.add(BarEntry(2f, 4f))
-        entries.add(BarEntry(3f, 6f))
-        entries.add(BarEntry(4f, 8f))
-        entries.add(BarEntry(5f, 10f))
-        entries.add(BarEntry(6f, 12f))
-        entries.add(BarEntry(7f, 14f))
-        entries.add(BarEntry(8f, 16f))
-        entries.add(BarEntry(9f, 18f))
-        entries.add(BarEntry(10f, 20f))
-
-        val barDataSet = BarDataSet(entries, "Tasks")
+        val barDataSet = BarDataSet(entries, "Tasks in past 7  days")
         val barData = BarData(barDataSet)
 
 
-        val barChart = view.findViewById<BarChart>(R.id.bargraph)
+        barChart = view.findViewById(R.id.bargraph)
+
 
         // Set the legend
         val legend = barChart.legend
@@ -105,17 +101,18 @@ class HomeFragmentation : Fragment() {
         barChart.data = barData
         barDataSet.setColor(Color.GREEN) // Set the color of the bars
         barChart.invalidate() // refreshes the chart
+        LoadDatas(barChart, barData, barDataSet)
+        TaskDone = view.findViewById(R.id.done_task_count)
 
-
-        val circularProgressBar = view.findViewById<CircularProgressBar>(R.id.circularProgressBar)
+        circularProgressBar = view.findViewById<CircularProgressBar>(R.id.circularProgressBar)
         circularProgressBar.apply {
             // Set Progress
-            progress = 100f
+            //progress = 100f
             // or with animation
-            setProgressWithAnimation(100f, 1000) // =1s
+            //setProgressWithAnimation(100f, 3000) // =1s
 
             // Set Progress Max
-            progressMax = 200f
+            progressMax = 100f
 
             // Set ProgressBar Color
             progressBarColor = Color.GREEN
@@ -139,13 +136,101 @@ class HomeFragmentation : Fragment() {
             roundBorder = true
             startAngle = 180f
             progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
-            percentage.setText("$progress %")
         }
+        circularProgressBar2 = view.findViewById<CircularProgressBar>(R.id.circularProgressBar2)
+        circularProgressBar2.apply {
+            // Set Progress
+            //progress = 100f
+            // or with animation
+            //setProgressWithAnimation(100f, 3000) // =1s
+
+            // Set Progress Max
+            progressMax = 100f
+
+            // Set ProgressBar Color
+            progressBarColor = Color.GREEN
+            // or with gradient
+            progressBarColorStart = Color.GREEN
+            progressBarColorEnd = Color.GREEN
+            progressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+            // Set background ProgressBar Color
+            backgroundProgressBarColor = Color.GREEN
+            // or with gradient
+            backgroundProgressBarColorStart = Color.WHITE
+            backgroundProgressBarColorEnd = Color.WHITE
+            backgroundProgressBarColorDirection = CircularProgressBar.GradientDirection.TOP_TO_BOTTOM
+
+            // Set Width
+            progressBarWidth = 7f // in DP
+            backgroundProgressBarWidth = 3f // in DP
+
+            // Other
+            roundBorder = true
+            startAngle = 180f
+            progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+        }
+        LoadStats()
 
 
         return view
     }
 
+    private fun LoadDatas(barChart: BarChart?, barData: BarData, barDataSet: BarDataSet) {
+        val sevenDaysAgo = LocalDate.now().minusDays(7)
+        val CollectionReference1 = db.collection("Task")
+        val query1 = CollectionReference1.whereEqualTo("Status", "Done").whereGreaterThanOrEqualTo("Date", sevenDaysAgo)
+        query1.get().addOnSuccessListener {
+            val pending = it.size()
+
+            for (i in 0 until pending) {
+                barDataSet.addEntry(BarEntry(i.toFloat(), pending.toFloat()))
+                barChart?.data = barData
+                barChart?.invalidate()
+            }
+
+
+        }
+
+    }
+
+    fun LoadStats(){
+        var retriveCount1:Double = 0.00
+        var retriveCount2:Double = 0.00
+        val CollectionReference1 = db.collection("Task")
+        val query1 = CollectionReference1.whereEqualTo("Status", "Pending")
+        query1.get().addOnSuccessListener {
+            val pending = it.size()
+            retriveCount1 = pending.toDouble()
+
+            val query2 = CollectionReference1.whereEqualTo("Status", "Done")
+            query2.get().addOnSuccessListener {
+                val done = it.size()
+                retriveCount2 = done.toDouble()
+
+                val total = retriveCount1 + retriveCount2
+                val percentages1 = retriveCount2 / total * 100
+                val percentages2 = retriveCount1 / total * 100
+
+                val stringg1 = percentages1.toString()
+                val stringg2 = percentages2.toString()
+                val subStr = subString(stringg1)
+                val subStr2 = subString(stringg2)
+                val FloatPercentages1 = percentages1.toFloat()
+                val FloatPercentages2 = percentages2.toFloat()
+
+                percentage.text = "$subStr %"
+                TaskDone.text = "$subStr2 %"
+                circularProgressBar.setProgressWithAnimation(FloatPercentages1, 3000)
+                circularProgressBar2.setProgressWithAnimation(FloatPercentages2, 3000)
+                // Update the UI here with the calculated percentage
+            }
+        }
+    }
+
+    fun subString (string: String): String {
+        return string.substring(0, 5)
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
