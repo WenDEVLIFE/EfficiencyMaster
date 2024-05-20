ac9bde534bbd9a414dc1292851eb986efb966805
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-05-20 13:17:58 +0800

    Change the recycleview layout i n GroupTask to LinearLayoutManager 👾

--- app/src/main/java/com/example/efficiencymaster/GroupTask.kt
+++ app/src/main/java/com/example/efficiencymaster/GroupTask.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import classes.GroupTaskInfo
 import classes.NonInterceptingLinearLayoutManager
@@ -162,7 +163,7 @@ class GroupTask : Fragment(), GroupTaskAdapter.OnCancelListener {
         }
 
         recyclerView = view.findViewById(R.id.recycler_view)
-        recyclerView.layoutManager = NonInterceptingLinearLayoutManager(requireContext())
+        recyclerView.setLayoutManager(LinearLayoutManager(requireContext()))
         grouptaskList = ArrayList()
         adapters =  GroupTaskAdapter(grouptaskList)
         recyclerView.adapter = adapters
