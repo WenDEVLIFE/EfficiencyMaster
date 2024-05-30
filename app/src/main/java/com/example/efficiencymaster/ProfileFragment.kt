057a7fc55a6eb3426fb8237eff3457d03b72aaa6
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-05-30 09:19:02 +0800

    addedg rid   layout dependencies   and unfinished fragment_profile.xml  👾

--- app/src/main/java/com/example/efficiencymaster/ProfileFragment.kt
+++ app/src/main/java/com/example/efficiencymaster/ProfileFragment.kt
@@ -34,7 +34,9 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view =  inflater.inflate(R.layout.fragment_profile, container, false)
+
+        return view
     }
 
     companion object {
